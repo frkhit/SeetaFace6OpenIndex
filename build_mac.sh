7758039e3faedb26bbfb06958404addc203f2ad8
frkhit <frkhit@gmail.com>
2022-04-02 09:12:28 +0800

    build lib for mac

---
+++ build_mac.sh
@@ -0,0 +1,20 @@
+#/bin/env bash
+
+
+cd OpenRoleZoo/craft/ && bash build.linux.x64.sh && cd ../.. && \
+cd SeetaAuthorize/craft/ && bash build.linux.x64.sh && cd ../.. && \
+cd TenniS/craft/ && bash build.linux.x64.sh && cd ../.. && \
+cd Landmarker/craft/ && bash build.linux.x64.sh && cd ../.. && \
+cd FaceAntiSpoofingX6/craft/ && bash build.linux.x64.sh && cd ../.. && \
+cd PoseEstimator6/craft/ && bash build.linux.x64.sh && cd ../.. && \
+cd QualityAssessor3/craft/ && bash build.linux.x64.sh && cd ../.. && \
+cd SeetaEyeStateDetector/craft/ && bash build.linux.x64.sh && cd ../.. && \
+cd FaceBoxes/craft/ && bash build.linux.x64.sh && cd ../.. && \
+cd SeetaGenderPredictor/craft/ && bash build.linux.x64.sh && cd ../.. && \
+cd FaceRecognizer6/craft/ && bash build.linux.x64.sh && cd ../.. && \
+cd SeetaAgePredictor/craft/ && bash build.linux.x64.sh && cd ../.. && \
+cd SeetaMaskDetector/craft/ && bash build.linux.x64.sh && cd ../.. && \
+cd FaceTracker6/craft/ && bash build.linux.x64.sh && cd ../.. && \
+echo "Done!"
+
+
